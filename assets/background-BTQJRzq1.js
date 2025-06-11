b78dd0551ef530c217c5c9737c32b51755b89e43
rivasmig <rivasmig@oregonstate.edu>
2025-06-11 01:55:49 -0700

    Updates

---
+++ assets/background-BTQJRzq1.js
@@ -0,0 +1,22 @@
+const e=`---\r
+title: "Background"\r
+isImageSection: false\r
+---\r
+\r
+# Background\r
+\r
+Gravity Falls is a critically acclaimed animated series created by Alex Hirsch that aired from 2012 to 2016. The show follows twins Dipper and Mabel Pines as they spend their summer with their great-uncle Stan in the mysterious town of Gravity Falls, Oregon.\r
+\r
+![Gravity Falls Main Characters](https://image.tmdb.org/t/p/original/cvJPd911PMS7cY8ajwZlnF0xClj.jpg "Gravity Falls cast - Image credit: TMDB")\r
+\r
+## Why Analyze Gravity Falls?\r
+\r
+The series is renowned for its sophisticated storytelling and complex narrative structure that rewards careful analysis. Unlike typical animated shows, Gravity Falls employs extensive foreshadowing and interconnected plotlines that make it ideal for academic study.\r
+\r
+## Mysteries and Foreshadowing\r
+\r
+Gravity Falls contains numerous embedded clues and hidden messages throughout episodes, including visual Easter eggs, cryptographic puzzles, and symbolic references that gain meaning in later episodes. The central mystery revolves around supernatural journals documenting strange phenomena in the town.\r
+\r
+![Ciphers and Mystery](/GravityFallsResearchAssets/coolBillPic.jpg "Bill Cipher mysteries - Image credit: vrogue.co")\r
+\r
+The show's mysteries are carefully woven throughout the series, with seemingly minor details in early episodes becoming crucial plot points later, creating a viewing experience where episodes are deeply interconnected through narrative threads.`;export{e as default};
---
+++ assets/methods-QYDbLQW4.js
@@ -0,0 +1,14 @@
+const e=`---\r
+title: "Methods"\r
+isImageSection: false\r
+---\r
+\r
+# Methods\r
+\r
+The main method for analyzing and collecting our data was that we each watched through 10 episodes, looked at any elements that would foreshadow future events, paid attention to any call-backs or continuity nods that were featured within our 10 episodes, and integrated our findings into our data, which led to our conclusion.\r
+\r
+![Data Collection Process](/GravityFallsResearchAssets/GF_Degree.png)\r
+\r
+We also looked at the Gravity Falls wiki page for any pieces of information that we may have missed. After tightening everything up, I'd say we managed to incorporate practically all the foreshadowing elements that are found within Gravity Falls and connected media.\r
+\r
+![Episode Analysis Example](/GravityFallsResearchAssets/GF_Degree.png)`;export{e as default};
